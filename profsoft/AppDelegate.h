858d72ec8639ad32659fcfdf82272d317c78d600
марк <mark@macbook-pro-mark.local>
2018-03-12 20:00:22 +0400

    Initial Commit

---
+++ profsoft/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  profsoft
+//
+//  Created by марк  on 12.03.2018.
+//  Copyright © 2018 марк . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ profsoft/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  profsoft
+//
+//  Created by марк  on 12.03.2018.
+//  Copyright © 2018 марк . All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
